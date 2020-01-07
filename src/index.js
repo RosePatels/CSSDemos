0cd57f71aecb6a4d4a8ead083789290340d5bacf
shree <shree1795@gmail.com>
2020-01-07 17:27:57 -0500

    Adds flip card animations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles/reset.scss";
 import "./styles/index.scss";
 import "./styles/button-background-animations.scss";
+import "./styles/flip-card-animations.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   
