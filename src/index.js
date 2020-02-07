1241cb5b34234cb6f41be173a2009a2ab4162c18
shree <shree1795@gmail.com>
2020-02-07 17:14:45 -0500

    Updates CSS Demos title animation and adds typewriter effect

--- src/index.js
+++ src/index.js
@@ -12,26 +12,26 @@ window.addEventListener("DOMContentLoaded", () => {
     let tileContainer = document.querySelector('.tile-tilt-container');
     let tileTiltCard = document.querySelector('.tile-tilt');
 
-    function tilt(event){
+    // function tilt(event){
 
-        let offsetXReset = event.offsetX - 425;
-        let offsetYReset = event.offsetY - 200;
-        let xgap = ((offsetXReset)/ tileContainer.offsetWidth);
-        let ygap = ((offsetYReset)/ tileContainer.offsetHeight);
+    //     let offsetXReset = event.offsetX - 425;
+    //     let offsetYReset = event.offsetY - 200;
+    //     let xgap = ((offsetXReset)/ tileContainer.offsetWidth);
+    //     let ygap = ((offsetYReset)/ tileContainer.offsetHeight);
 
-        if (offsetXReset >= 0 && offsetYReset <= 0){
-            tileTiltCard.style.transform = `rotateX(${-xgap}deg) rotateY(${ygap}deg)`;
-        } else if (offsetXReset <= 0 && offsetYReset <= 0){
-            tileTiltCard.style.transform = `rotateX(${xgap}deg) rotateY(${-ygap}deg)`;
-        } else if (offsetXReset <= 0 && offsetYReset >= 0){
-            tileTiltCard.style.transform = `rotateX(${-xgap}deg) rotateY(${ygap}deg)`;
-        } else if (offsetXReset >= 0 && offsetYReset >= 0){
-            tileTiltCard.style.transform = `rotateX(${xgap}deg) rotateY(${-ygap}deg)`;
-        }
+    //     if (offsetXReset >= 0 && offsetYReset <= 0){
+    //         tileTiltCard.style.transform = `rotateX(${-xgap}deg) rotateY(${ygap}deg)`;
+    //     } else if (offsetXReset <= 0 && offsetYReset <= 0){
+    //         tileTiltCard.style.transform = `rotateX(${xgap}deg) rotateY(${-ygap}deg)`;
+    //     } else if (offsetXReset <= 0 && offsetYReset >= 0){
+    //         tileTiltCard.style.transform = `rotateX(${-xgap}deg) rotateY(${ygap}deg)`;
+    //     } else if (offsetXReset >= 0 && offsetYReset >= 0){
+    //         tileTiltCard.style.transform = `rotateX(${xgap}deg) rotateY(${-ygap}deg)`;
+    //     }
 
 
-    }
-    tileContainer.addEventListener('mousemove', tilt);
+    // }
+    // tileContainer.addEventListener('mousemove', tilt);
 
     //img spotlight cursor animation
 
@@ -49,4 +49,47 @@ window.addEventListener("DOMContentLoaded", () => {
     //     rotation[i].classList.add('rotate-word-animation')
     // }
 
+
+    //typewriter text animation
+    //Created with the help of Traversy Media: https://www.youtube.com/watch?v=POX3dT-pB4E
+    let testing = document.getElementById('testing');
+    let words = ["World!", "Planet!", "Globe!", "Earth!", "Galaxy!", "Universe!"];
+    let wordsIndex = 0;
+    let wordIdx = 0;
+    let insertStr = "";
+    let removeWord = false;
+    let timer = 500;
+
+    function typewriter(){
+        let actualWordIdx = wordsIndex % words.length;
+
+        if (removeWord){
+            timer = 100;
+            insertStr = insertStr.slice(0, insertStr.length - 1);
+            if(insertStr.length === 0){
+                removeWord = false;
+                timer = 500;
+            }
+        }
+        else{
+            if (insertStr.length === words[actualWordIdx].length){
+                wordsIndex += 1;
+                wordIdx = 0;
+                removeWord = true;
+                timer = 2000;
+            } else {
+                insertStr += words[actualWordIdx][wordIdx];
+                wordIdx += 1;
+                timer = 500;
+            }
+        }     
+
+        testing.innerHTML = insertStr;
+
+        setTimeout(() => typewriter(), timer);
+    }
+
+    typewriter();
+
 });
+
